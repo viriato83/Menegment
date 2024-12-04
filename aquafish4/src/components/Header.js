d7a10e9173e83331529fff556cb0b4a58e5f46e8
viriato83 <157888460+viriato83@users.noreply.github.com>
2024-12-04 21:36:33 +0200

    Update Header.js

--- aquafish4/src/components/Header.js
+++ aquafish4/src/components/Header.js
@@ -10,7 +10,7 @@ export default function Header(){
          
         </div>
         <div className="logo">
-            <img src="../logo_white-removebg2.png" alt="" width="130px"/>
+             <img src="logo_white-removebg2.png" alt="" width="130px"/>
         </div>
         <h1>Aquafish Management System</h1>
         <button className="sair" onClick={(Sair)}>Sair</button>
